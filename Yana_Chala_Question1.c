a2ed16830d4cd45b7180ca3c95e4c7b50b6e5c3b
Yana Chala <ydc223@nyu.edu>
2018-03-01 10:44:52 +0400

    heap memory

--- Yana_Chala_Question1.c
+++ Yana_Chala_Question1.c
@@ -8,13 +8,15 @@ extern char __etext, _edata, _end;
 
 int main(void)
 {
+  int m;
+  int m = 5;
   printf("    program text start (executable_start) 0x%lx\n", (unsigned long)&__executable_start);
   printf("    first address following program text end (etext) 0x%lx\n", (unsigned long)&__etext);
   printf("    initialized data (edata)  0x%lx\n",(unsigned long) &_edata);
   printf("    uninitialized data (end)  0x%lx\n", (unsigned long)&_end);
   char *pt;
   pt=(char*)malloc(10);
-  printf("    address of heap start is 0x%p.\n", pt);
+  printf("    address of heap start is %p.\n", pt);
   printf("    uninitialized data (end) after allocating memory 0x%lx\n", (unsigned long)&_end);
 
   return 0;
